041638bb02f704440cb102c44c1431b1ad1a2096
yuji0276 <yujikami0110@gmail.com>
2025-12-21 16:49:55 +0900

    feat:エンターキー押下時に処理を開始

--- main.py
+++ main.py
@@ -14,41 +14,52 @@ for row_idx, row_data in enumerate(MAP_LAYOUT):
         all_sprites.add(cell)
 all_sprites.draw(screen)
 
+game_start = False
 # ゲームループ
 while running:
-    # イベント処理（閉じるボタンが押されたか）
+    # イベント処理
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # 世代交代
-    for row_idx, row_data in enumerate(MAP_LAYOUT):
-        for col_idx, cell_type in enumerate(row_data):
-            count = 0
-            #隣接するセルの探索
-            for row_offset, col_offset in NEIBOR_OFFSETS:
-                neighbor_row = row_idx + row_offset
-                neighbor_col = col_idx + col_offset
-                neighbor_cell_type = 0
-                #境界チェック
-                if 0 <= neighbor_row < len(MAP_LAYOUT) and 0 <= neighbor_col < len(MAP_LAYOUT[0]):
-                    neighbor_cell_type = MAP_LAYOUT[neighbor_row][neighbor_col]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                game_start = True
+
+    if not game_start:
+        pass
+    else:
+        # 世代交代
+        for row_idx, row_data in enumerate(MAP_LAYOUT):
+            for col_idx, cell_type in enumerate(row_data):
+                count = 0
+                #隣接するセルの探索
+                for row_offset, col_offset in NEIBOR_OFFSETS:
+                    neighbor_row = row_idx + row_offset
+                    neighbor_col = col_idx + col_offset
+                    neighbor_cell_type = 0
+                    #境界チェック
+                    if 0 <= neighbor_row < len(MAP_LAYOUT) and 0 <= neighbor_col < len(MAP_LAYOUT[0]):
+                        neighbor_cell_type = MAP_LAYOUT[neighbor_row][neighbor_col]
+                    else:
+                        continue
+                    if neighbor_cell_type == 1:
+                        count += 1
+                if MAP_LAYOUT[row_idx][col_idx] == 1:
+                    if count == 2 or count == 3:
+                        NEXT_LAYOUT[row_idx][col_idx] = 1
+                    else:
+                        NEXT_LAYOUT[row_idx][col_idx] = 0
                 else:
-                    continue
-                if neighbor_cell_type == 1:
-                    count += 1
-            if NEXT_LAYOUT[row_idx][col_idx] == 1:
-                if count == 2 or count == 3:
-                    NEXT_LAYOUT[row_idx][col_idx] = 1
-                else:
-                    NEXT_LAYOUT[row_idx][col_idx] = 0
-            else:
-                if count == 3:
-                    NEXT_LAYOUT[row_idx][col_idx] = 1
-                else:
-                    NEXT_LAYOUT[row_idx][col_idx] = 0
-    # 世代交代
-    MAP_LAYOUT = copy.deepcopy(NEXT_LAYOUT)
+                    if count == 3:
+                        NEXT_LAYOUT[row_idx][col_idx] = 1
+                    else:
+                        NEXT_LAYOUT[row_idx][col_idx] = 0
+        
+        # 世代交代
+        MAP_LAYOUT = copy.deepcopy(NEXT_LAYOUT)
+
     # 画面を更新して変更を反映
+    screen.fill((113,115,117)) # Clear screen every frame
     all_sprites.empty()
 
     # グループ内の全てのセルを描画する
@@ -64,7 +75,9 @@ while running:
         x = c * Cell.size
         pygame.draw.line(screen, (50, 50, 50), (x, 0), (x,SCREEN_HEIGHT))
     pygame.display.flip()
+
     # フレームレートの制御
-    clock.tick(2)
+    clock.tick(30)
+
 # ゲーム終了処理
 pygame.quit()
